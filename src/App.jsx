0d326f72ec340e23e0eaad6f405eaa3d5d0a74e6
Annie Tran <tranthilananh105@gmail.com>
2024-11-15 16:54:56 +0200

    update App component to show drawer

--- src/App.jsx
+++ src/App.jsx
@@ -10,58 +10,106 @@ import {
   ListItemIcon,
   ListItemText,
   Drawer,
+  CssBaseline,
 } from "@mui/material";
-import { Inbox as InboxIcon, Mail as MailIcon } from "@mui/icons-material";
-
-import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Face2Icon from "@mui/icons-material/Face2";
+import SportsGymnasticsIcon from "@mui/icons-material/SportsGymnastics";
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
+import MenuIcon from "@mui/icons-material/Menu";
+import Customers from "./components/Customers";
+import Trainings from "./components/Trainings";
+import Statistics from "./components/Statistics";
+
+const drawerWidth = 240; // Width of the drawer
 
 function App() {
   const [open, setOpen] = useState(false);
+
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {["Customer", "Training", "Statistics"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+    <List>
+      {[
+        { text: "Customer", icon: <Face2Icon />, path: "/customer" },
+        { text: "Training", icon: <SportsGymnasticsIcon />, path: "/training" },
+        { text: "Statistics", icon: <LeaderboardIcon />, path: "/statistics" },
+      ].map(({ text, icon, path }) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton component={Link} to={path}>
+            <ListItemIcon>{icon}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   );
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={toggleDrawer(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Personal Trainer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-    </Box>
+    <Router>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            backgroundColor: "#f08080",
+            width: open ? `calc(100% - ${drawerWidth}px)` : "100%",
+            ml: open ? `${drawerWidth}px` : 0,
+            transition: "margin 0.3s ease-in-out",
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(!open)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 500 }}>
+              Personal Trainer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="persistent"
+          anchor="left"
+          open={open}
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Box sx={{ overflow: "auto" }}>{DrawerList}</Box>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            width: open ? `calc(100% - ${drawerWidth}px)` : "100%",
+            transition: "margin 0.3s ease-in-out",
+            ml: open ? `${drawerWidth}px` : 0,
+          }}
+        >
+          <Toolbar />
+          <Routes>
+            <Route path="/customer" element={<Customers />} />
+            <Route path="/training" element={<Trainings />} />
+            <Route path="/statistics" element={<Statistics />} />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
   );
 }
 
